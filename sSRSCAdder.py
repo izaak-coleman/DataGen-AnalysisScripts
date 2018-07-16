68696228ab8f7862ad4dcd737b4d7992b2bca7fc
Izaak Coleman <ic711@potoo01.doc.ic.ac.uk>
2018-07-16 16:25:53 +0100

    BUG_FIX: mlist SNV locations were off by one

--- sSRSCAdder.py
+++ sSRSCAdder.py
@@ -2,11 +2,11 @@ import sys
 import random
 from subprocess import check_output
 
-def get_valid_ranges(chromosome, n_clusters, fa_len, bed_path):
+def get_valid_ranges(chromosome, n_clusters, fa_len, bed_path, fasta):
   random.seed()
   ranges = []
   while len(ranges) != n_clusters:
-    r = random.randint(0, fa_len-1)
+    r = random.randint(0, fa_len-501)
     r = (r, r+500)
 
     # Check range is not within a centromere
@@ -30,6 +30,9 @@ def get_valid_ranges(chromosome, n_clusters, fa_len, bed_path):
       print('Range: %d, %d failed overlap filter' %(r[0],r[1]))
       continue
 
+    # Avoid 'N' regions
+    if 'N' in fasta[r[0] : r[1]]:
+      continue
     # Passed.
     ranges.append(r)
   return ranges
@@ -56,15 +59,16 @@ def add_clusters(chromosome, fasta, base_name, ranges, max_k):
         m = random.randint(0,100)
       c,t,fasta = addSNV(l+m, fasta)
       added_snvs.append(m)
-      sSRSC_lists[k-1].append(chromosome + "\t%d\t%s\t%s\n" % (l+m, c, t))
+      sSRSC_lists[k-1].append(chromosome + "\t%d\t%s\t%s\n" % (l+m+1, c, t))
 
   # Write data.
   fasta = ''.join(fasta)
-  fasta_file = base_name + 'sSRSC.fasta'
+  fasta_file = base_name + '.sSRSC.fasta'
   with open(fasta_file,'w') as f:
-    f.write('\n'.join([fasta[i:i+80] for i in range(0,len(fasta), 100)]))
+    f.write('>' + chromosome + '\n')
+    f.write('\n'.join([fasta[i:i+80] for i in range(0,len(fasta), 80)]))
   for k in range(1,max_k+1):
-    mlist = base_name + str(k) + '.mlist'
+    mlist = base_name + '.' + str(k) + '.mlist'
     with open(mlist,'w') as f:
       f.write(''.join(sSRSC_lists[k-1]))
 
@@ -72,7 +76,7 @@ def addSNV(pos, fasta):
   control = fasta[pos]
   bases = 'ATCG'.replace(control,'')
   tumour = random.choice(bases)
-  fasta = fasta[pos] = tumour
+  fasta[pos] = tumour
   return control, tumour, fasta
   
 
@@ -81,11 +85,16 @@ def main():
   if len(sys.argv) != 6:
     print("Usage: <exe> <p.fa> <m.fa> <n_clusters> <bed_path> <max_k>")
     sys.exit(1)
+  p_basename = sys.argv[1]
+  p_basename = p_basename[p_basename.rfind('/')+1:]
+  m_basename = sys.argv[2]
+  m_basename = m_basename[m_basename.rfind('/')+1:]
+
   chromosome, p_fasta = load_fasta(sys.argv[1])
-  m_fasta = load_fasta(sys.argv[2])
-  ranges = get_valid_ranges(chromosome, int(sys.argv[3]), len(p_fasta), sys.argv[4])
-  add_clusters(chromosome, p_fasta, sys.argv[1][-6], ranges[:len(ranges)/2], int(sys.argv[5]))
-  add_clusters(chromosome, m_fasta, sys.argv[2][-6], ranges[len(ranges)/2:], int(sys.argv[5]))
+  _, m_fasta = load_fasta(sys.argv[2])
+  ranges = get_valid_ranges(chromosome, int(sys.argv[3]), len(p_fasta), sys.argv[4], m_fasta)
+  add_clusters(chromosome, p_fasta, p_basename[:-3], ranges[:len(ranges)/2], int(sys.argv[5]))
+  add_clusters(chromosome, m_fasta, m_basename[:-3], ranges[len(ranges)/2:], int(sys.argv[5]))
 
 
 if __name__ == '__main__':
